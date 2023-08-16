c680d3989c6c5cc33e8e250c4ad91e35a32fff76
Salomao <salomao.machado.75@gmail.com>
2023-08-15 23:07:16 -0300

    criando componente para evitar repetição de codigo

---
+++ src/app/shared/botao-controle/botao-controle.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-botao-controle',
+  templateUrl: './botao-controle.component.html',
+  styleUrls: ['./botao-controle.component.scss']
+})
+export class BotaoControleComponent {
+
+}
