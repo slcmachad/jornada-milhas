e3bfbcbfa292d41d6a2cecdfc5fb8eadfd665641
Salomao <salomao.machado.75@gmail.com>
2023-08-15 23:10:30 -0300

    <fim> Concluido curso adicionando componentes ao modal.

--- src/app/shared/botao-controle/botao-controle.component.ts
+++ src/app/shared/botao-controle/botao-controle.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-botao-controle',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./botao-controle.component.scss']
 })
 export class BotaoControleComponent {
-
+  @Input() operacao: 'incrementar' | 'decrementar' = "incrementar";
+  @Input() src = '';
+  @Input() alt = '';
 }
