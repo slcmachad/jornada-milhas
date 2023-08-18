3f060bfdeea85cf573fa982f13c7bba76718e186
Salomao <salomao.machado.75@gmail.com>
2023-08-18 17:31:17 -0300

    alterando metodos e utilizando prefixos nos componentes.

--- src/app/shared/form-busca/dropdown-uf/dropdown-uf.component.ts
+++ src/app/shared/form-busca/dropdown-uf/dropdown-uf.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dropdown-uf',
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class DropdownUfComponent {
 
+  @Input() label: string = '';
+  @Input() iconePrefixo: string = '';
 
 
 }
