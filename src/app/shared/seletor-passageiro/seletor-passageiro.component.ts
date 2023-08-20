a55af2ad948122c5bdd6e0a7357b0756ddf98a24
Salomao <salomao.machado.75@gmail.com>
2023-08-19 22:00:52 -0300

    verificando se o componente recém-criado está sendo controlado corretamente pelo formControl.

--- src/app/shared/seletor-passageiro/seletor-passageiro.component.ts
+++ src/app/shared/seletor-passageiro/seletor-passageiro.component.ts
@@ -1,10 +1,17 @@
-import { Component, Input } from '@angular/core';
-import { ControlValueAccessor } from '@angular/forms';
+import { Component, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-seletor-passageiro',
   templateUrl: './seletor-passageiro.component.html',
-  styleUrls: ['./seletor-passageiro.component.scss']
+  styleUrls: ['./seletor-passageiro.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SeletorPassageiroComponent),
+      multi: true
+    }
+  ]
 })
 export class SeletorPassageiroComponent implements ControlValueAccessor{
 
