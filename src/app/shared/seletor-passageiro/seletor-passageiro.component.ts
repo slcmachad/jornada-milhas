a272b20ffedfaef53c08985e620f073f8a5d7673
Salomao <salomao.machado.75@gmail.com>
2023-08-19 21:41:52 -0300

    implementando controlValueAccessor e iniciando metodos onChange e onTouch.

--- src/app/shared/seletor-passageiro/seletor-passageiro.component.ts
+++ src/app/shared/seletor-passageiro/seletor-passageiro.component.ts
@@ -1,13 +1,33 @@
 import { Component, Input } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-seletor-passageiro',
   templateUrl: './seletor-passageiro.component.html',
   styleUrls: ['./seletor-passageiro.component.scss']
 })
-export class SeletorPassageiroComponent {
+export class SeletorPassageiroComponent implements ControlValueAccessor{
 
   @Input() titulo: string = '';
   @Input() subTitulo: string = '';
+
+  value: number = 0
+  onChange = () => {}
+  onTouch = () => {}
+
+  writeValue(obj: any): void {
+    throw new Error('Method not implemented.');
+  }
+  registerOnChange(fn: any): void {
+    throw new Error('Method not implemented.');
+  }
+  registerOnTouched(fn: any): void {
+    throw new Error('Method not implemented.');
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
+  }
+
+
 
 }
