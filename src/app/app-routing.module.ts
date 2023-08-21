694237667911421b47a0ea6d7c1790ccd8ccb42a
Salomao <salomao.machado.75@gmail.com>
2023-08-20 22:24:16 -0300

    criando rota do componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './pages/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
@@ -6,6 +7,10 @@ const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   }
 ];
 
