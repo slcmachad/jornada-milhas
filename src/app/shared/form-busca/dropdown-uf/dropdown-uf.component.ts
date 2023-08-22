399cec335ce5e19d76b41f750d6d5013ce990028
Salomao <salomao.machado.75@gmail.com>
2023-08-21 22:59:25 -0300

    ajustando formbusca e dropdown

--- src/app/shared/form-busca/dropdown-uf/dropdown-uf.component.ts
+++ src/app/shared/form-busca/dropdown-uf/dropdown-uf.component.ts
@@ -1,6 +1,7 @@
 import { UnidadeFederativa } from './../../../core/types/type';
 import { UnidadeFederativaService } from './../../../core/services/unidade-federativa.service';
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dropdown-uf',
@@ -11,6 +12,9 @@ export class DropdownUfComponent implements OnInit{
 
   @Input() label: string = '';
   @Input() iconePrefixo: string = '';
+  @Input() placeholder: string = '';
+  @Input() control!: FormControl;
+
 
   unidadesFederativas: UnidadeFederativa[] =  []
 
