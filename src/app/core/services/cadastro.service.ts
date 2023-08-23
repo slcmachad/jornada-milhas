23d80c0033a0a0fdc49fb73bbccc819f0ef32c4c
Salomao <salomao.machado.75@gmail.com>
2023-08-22 22:37:42 -0300

    Criando um serviço CadastroService que será provido a todo o aplicativo.

---
+++ src/app/core/services/cadastro.service.ts
@@ -0,0 +1,19 @@
+import { Observable } from 'rxjs';
+import { PessoaUsuaria } from './../types/type';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CadastroService {
+
+  private apiUrl: string = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  cadastrar(pessoaUsuaria: PessoaUsuaria): Observable<PessoaUsuaria>{
+    return this.http.post<PessoaUsuaria>(`${this.apiUrl}/auth/cadastro`, pessoaUsuaria)
+  }
+}
