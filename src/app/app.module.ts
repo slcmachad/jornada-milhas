452f578038f5043f07c527f6935fc04ebe448161
Salomao <salomao.machado.75@gmail.com>
2023-08-24 21:29:29 -0300

    criando component para a parte de perfil e adicionando rotas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,6 +40,7 @@ import { MatRadioModule } from '@angular/material/radio';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
 
 @NgModule({
   declarations: [
@@ -61,7 +62,8 @@ import { CadastroComponent } from './pages/cadastro/cadastro.component';
     DepoimentosComponent,
     LoginComponent,
     FormBaseComponent,
-    CadastroComponent
+    CadastroComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
